08fd8544dc4d446fa8b33680040a3cba60fdde75
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-16 22:59:02 +0700

    Add reset test

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -8,6 +8,12 @@ describe('unicafe reducer', () => {
     bad: 0,
   };
 
+  const anotherState = {
+    good: 4,
+    ok: 5,
+    bad: 3,
+  };
+
   test('should return a proper initial state when called with undefined state', () => {
     const state = {};
     const action = {
@@ -71,6 +77,21 @@ describe('unicafe reducer', () => {
       good: 0,
       ok: 0,
       bad: 1,
+    });
+  });
+
+  test('reset state', () => {
+    const action = {
+      type: 'RESET',
+    };
+    const state = anotherState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0,
     });
   });
 });
