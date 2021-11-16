ae4876cd274f18d23a16e6ebe3a950eccb24bf44
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-17 00:28:05 +0700

    Import anecdote list and form components to App.js

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,44 +1,13 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { voteAnecdote, createAnecdote } from './reducers/anecdoteReducer';
+import AnecdoteList from './components/AnecdoteList';
+import AnecdoteForm from './components/AnecdoteForm';
 
 const App = () => {
-  const anecdotes = useSelector((state) =>
-    state.sort((a, b) => b.votes - a.votes)
-  );
-  const dispatch = useDispatch();
-
-  const vote = (id) => {
-    console.log('vote', id);
-    dispatch(voteAnecdote(id));
-  };
-
-  const create = (e) => {
-    e.preventDefault();
-    const content = e.target.anecdote.value;
-    e.target.anecdote.value = '';
-    dispatch(createAnecdote(content));
-  };
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
-      <h2>create new</h2>
-      <form onSubmit={create}>
-        <div>
-          <input name="anecdote" />
-        </div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   );
 };
