083a5ce620b7752a0d091076596690fc2071a7f0
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-19 03:41:03 +0700

    Implement async/await create creation

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { createAnecdote } from '../reducers/anecdoteReducer';
-import anecdoteService from '../services/anecdoteService';
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
-  const create = async (e) => {
+  const create = (e) => {
     e.preventDefault();
     const content = e.target.anecdote.value;
-    try {
-      const response = await anecdoteService.createNew(content);
-      dispatch(createAnecdote(response));
-    } catch (error) {
-      console.log(error);
-    }
+    dispatch(createAnecdote(content));
     e.target.anecdote.value = '';
   };
   return (
